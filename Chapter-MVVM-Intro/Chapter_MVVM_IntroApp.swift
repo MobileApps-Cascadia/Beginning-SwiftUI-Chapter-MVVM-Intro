bde187ccd640d04290e521e90267b4bf10fb5134
Mike Panitz <michpa@hotmail.com>
2023-05-20 18:44:57 -0700

    Initial Commit

---
+++ Chapter-MVVM-Intro/Chapter_MVVM_IntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chapter_MVVM_IntroApp.swift
+//  Chapter-MVVM-Intro
+//
+//  Created by Mike Panitz on 5/20/23.
+//
+
+import SwiftUI
+
+@main
+struct Chapter_MVVM_IntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
