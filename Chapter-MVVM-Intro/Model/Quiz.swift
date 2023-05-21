1c0f38ef03667ca3c28cf92c84a8b6020c670893
Mike Panitz <michpa@hotmail.com>
2023-05-21 00:58:41 -0700

    Example solution of MVVM (hopefully - MVVM isn't clearly defined online)

--- Chapter-MVVM-Intro/Model/Quiz.swift
+++ Chapter-MVVM-Intro/Model/Quiz.swift
@@ -1,4 +1,14 @@
 class Quiz{
     
-
+    let questions: [String] = [
+        "From what is cognac made?",
+        "What is 7+7?",
+        "What is the capital of Vermont?"
+    ]
+    
+    let answers: [String] = [
+        "Grapes",
+        "14",
+        "Montpelier"
+    ]
 }
